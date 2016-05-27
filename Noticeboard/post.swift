b533ca954668653f6a7b521e0d23350ce79c3d9d
Edwin <edwin@edwins-macbook-pro.local>
2016-05-27 14:23:52 -0400

    Changes in UI

---
+++ Noticeboard/post.swift
@@ -0,0 +1,25 @@
+//
+//  post.swift
+//  Noticeboard
+//
+//  Created by Edwin on 27/05/16.
+//  Copyright © 2016 Edwin. All rights reserved.
+//
+
+import Foundation
+
+class Post {
+    
+    private var Imagepath:String
+    private var Title:String
+    private var Postdesc:String
+    
+    init(Imagepath:String, Title:String, Postdesc:String){
+    self.Imagepath = Imagepath
+    self.Title = Title
+    self.Postdesc = Postdesc
+        }
+    
+    
+    
+}

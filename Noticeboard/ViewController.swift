899ecbbf6e84f48ee5c9b6de7d09a365d40b4fa6
Edwin <edwin@edwins-macbook-pro.local>
2016-05-27 12:06:37 -0400

    Initial Commit

---
+++ Noticeboard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Noticeboard
+//
+//  Created by Edwin on 27/05/16.
+//  Copyright © 2016 Edwin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
